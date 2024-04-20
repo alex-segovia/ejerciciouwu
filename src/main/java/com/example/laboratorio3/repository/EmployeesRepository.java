3b6c72a6d6e181fd5b0363ae5af61bb620e7d484
Hineill <hineill.cespedes@pucp.edu.pe>
2024-04-19 20:39:15 -0500

    ga

--- src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
@@ -5,10 +5,12 @@ import com.example.laboratorio3.entity.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
-
+    List<Employees> findByEmployee_id(int employee_id);
 //COMPLETAR
 
 }
