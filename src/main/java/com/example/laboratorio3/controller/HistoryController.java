b06af5a9ffa3c51a6ebf5c9de7f37f53626cbfac
unbotcomotu <a20213852@pucp.edu.pe>
2024-04-19 20:33:30 -0500

    historial al 20%

--- src/main/java/com/example/laboratorio3/controller/HistoryController.java
+++ src/main/java/com/example/laboratorio3/controller/HistoryController.java
@@ -1,9 +1,24 @@
 package com.example.laboratorio3.controller;
 
 
-//COMPLETAR
-public class HistoryController {
-//COMPLETAR
+import com.example.laboratorio3.repository.EmployeesRepository;
+import jdk.internal.classfile.impl.BufferedCodeBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HistoryController {
+
+    final EmployeesRepository employeesRepository;
+
+    private HistoryController(EmployeesRepository employeesRepository){
+        this.employeesRepository=employeesRepository;
+    }
+
+    @GetMapping(value = "historial")
+    public String historial(Model model){
+        return "historial";
+    }
 
 }
