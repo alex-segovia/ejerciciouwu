940eff6caef508fdae1a37cf6af509b6757630ca
unbotcomotu <a20213852@pucp.edu.pe>
2024-04-19 20:55:04 -0500

    0 errorers :'v

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.example.laboratorio3.controller;
 
 import com.example.laboratorio3.entity.Employees;
+import com.example.laboratorio3.repository.EmployeesRepository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -17,6 +18,10 @@ import org.springframework.stereotype.Controller;
 @RequestMapping(value = {"/EmployeeIndex"}, method = RequestMethod.GET)
 public class EmployeeController {
 
+    final EmployeesRepository employeesRepository;
+    private EmployeeController(EmployeesRepository employeesRepository){
+        this.employeesRepository=employeesRepository;
+    }
     @GetMapping("/listaEmpleado")
     public String listar(Model model) {
         List<Employees> lista = employeesRepository.findAll();
--- src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
@@ -25,18 +25,11 @@ public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
     List<Employees> findByEmployee_id(int employee_id);
 
 
-    @Query(value = "select * from employees e inner join department d on e.department_id=d.department_id inner join job_history j on e.employee_id=j.employee_id where e.first_name like %?1% or e.last_name like %?1% or d.department_name like %?1% or j.employee_id  like %?1% ",nativeQuery = true)
+    @Query(value = "select * from employees e inner join department d on e.department_id=d.department_id inner join job_history j on e.employee_id=j.employee_id where e.first_name like %?1% or e.last_name like %?1% or d.department_name like %?1% or j.employee_id  like %?1% ", nativeQuery = true)
     List<Employees> searchEmployees(String text);
-    @Transactional
-    @Modifying
-    @Query(nativeQuery = true, value = "")
-    void actualizar
 
-
-
-
-
-public interface EmployeesRepository extends JpaRepository<Employees,Integer> {
-    @Query(nativeQuery = true,value = "select first_name, last_name, salary from employees where salary>15000")
+    @Query(nativeQuery = true, value = "select first_name, last_name, salary from employees where salary>15000")
     List<Employees> listarPorMayorSalario();
+
 }
+
